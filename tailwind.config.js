86a2307c871318e3a1b37a081e0a77f79d0374c7
Daniel <20095356@tafe.wa.edu.au>
2023-12-04 23:27:04 +0800

    feat(project): Added a mobild view responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     './*.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+      },
+    },
   },
   plugins: [],
 }
