7440ac7c3bd09bf4e78468e8d8aa90f4592b6157
Daniel <20095356@tafe.wa.edu.au>
2023-12-06 17:13:42 +0800

    fix(final project) This is my final commit, i just added a mobile layout navigation bar

--- assets/js/hamburgermenu.js
+++ assets/js/hamburgermenu.js
@@ -1,8 +1,17 @@
-document.querySelector('.hamburger-menu').addEventListener('click', function() {
-    var nav = document.querySelector('nav');
-    if (nav.style.display === 'none') {
-        nav.style.display = 'block';
-    } else {
-        nav.style.display = 'none';
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburgerMenu = document.getElementById('myHamburgerMenu');
+    const nav = document.querySelector('header nav');
+
+    // Function to be executed when the hamburger menu is clicked
+    function handleHamburgerClick() {
+        // Toggle the 'active' class on both the hamburger menu and the navigation menu
+        hamburgerMenu.classList.toggle('active');
+        nav.classList.toggle('active');
+
+        // You can add additional actions here if needed
+        console.log('Hamburger menu clicked!');
     }
+
+    // Add click event to the hamburger menu
+    hamburgerMenu.addEventListener('click', handleHamburgerClick);
 });
